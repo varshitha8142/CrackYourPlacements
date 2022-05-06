e923673698525da96efeccffa48c93433083333d
Varshitha <69466185+varshitha8142@users.noreply.github.com>
2022-05-06 14:44:50 +0530

    Time: 7513 ms (5.00%), Space: 15 MB (76.57%) - LeetHub

---
+++ 1-two-sum/1-two-sum.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                sum=nums[i]+nums[j]
+                if(sum!=target):
+                    j=j+1
+                else:
+                    return([i,j])
