7d192221783ad3bb3af172984cd58cdd9eef889c
Varshitha <69466185+varshitha8142@users.noreply.github.com>
2022-05-22 19:35:06 +0530

    Time: 76 ms (5.15%), Space: 13.8 MB (56.87%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.py
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.py
@@ -2,11 +2,15 @@ class Solution:
     def maxDepth(self, s: str) -> int:
         a=s
         x=[]
+        z=[]
         res=0
         for i in range(len(a)):
             if '('==a[i] :
                 x.append(a[i])
-                res = max(res, len(x))
+                z.append(len(x))
             elif ')'==a[i]:
                 x.pop()
-        return res
+        z.sort()
+        if len(z)>1:
+            return z[-1]
+        return len(z)
