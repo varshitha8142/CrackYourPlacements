8becb97258330c82f0d84e76e3c578d58f0b6a53
Varshitha <69466185+varshitha8142@users.noreply.github.com>
2022-09-20 15:08:24 +0530

    Time: 774 ms (5.07%), Space: 14.5 MB (56.97%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,11 +1,13 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         x=""
+        a=""
         for i in s:
             i=i.lower()
             if ord(i) in list(range(97,122)) or ord(i) in list(range(48,58)):
                 x+=i
-        if x==x[::-1]:
+                a=i+a
+        if x==a:
             return True
         else:
             return False
